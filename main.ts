3990bfca01b958da6525b3440410035adf9a8110
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-11-27 18:16:32 -0300

    Fix: função CadastrarVeiculo bem como alguns detalhes da classe Veiculo
    Descrição:
    
    Esta alteração corrige um bug na função CadastrarVeiculo que estava impedindo a correta inserção de alguns detalhes da classe Veiculo. Além disso, a classe Veiculo foi atualizada.

--- main.ts
+++ main.ts
@@ -1,73 +1,102 @@
-import Aluguel from './model/Aluguel';
-import { Locadora } from './model/Locadora';
-import Veiculo from './model/Veiculo';
+import Aluguel from "./model/Aluguel";
+import { Locadora } from "./model/Locadora";
+import Veiculo from "./model/Veiculo";
 import Cliente from "./model/Cliente";
 import * as readlineSync from 'readline-sync';
-import { read } from 'fs';
-
-Aluguel.inicializarContador();
-Cliente.inicializarContador();
-Cliente.cadastrarCliente('Zeno', '111.222.333-44', 'A');
+import { read } from "fs";
 
 const veiculos: Veiculo[] = [];
 
 function exibirMenu() {
-    console.log('\n');
-    console.log('======= MENU =======');
-    console.log('1. Cadastrar veículo');
-    console.log('2. Alugar veículo');
-    console.log('3. Devolver veículo');
-    console.log('4. Listar veículos disponíveis');
-    console.log('5. Listar veículos alugados');
-    console.log('6. Sair');
-    console.log('====================');
+	console.log("\n");
+	console.log("======= MENU =======");
+	console.log("1. Cadastrar veículo");
+	console.log("2. Alugar veículo");
+	console.log("3. Devolver veículo");
+	console.log("4. Listar veículos disponíveis");
+	console.log("5. Listar veículos alugados");
+	console.log("6. Sair");
+	console.log("====================");
 }
 
-let running = true;
+let running: boolean = true;
 while (running) {
-    exibirMenu();
-    const opcao = parseInt(readlineSync.question('Escolha uma opcao: '));
+	exibirMenu();
+	const opcao: string = readlineSync.question("Escolha uma opcao: ");
+	
+	// 	switch (opcao) {
+	// 		case "1":
+	// 			console.log(opcao);
+	// 			break;
+	// 		case "2":
+	// 			console.log(opcao);
+	// 			break;
+	// 		case "3":
+	// 			console.log(opcao);
+	// 			break;
+	// 		case "4":
+	// 			console.log(opcao);
+	// 			break;
+	// 		case "5":
+	// 			console.log(opcao);
+	// 			break;
+	// 		case "6":
+	// 			console.log(opcao);
+	// 			break;
+	// 		default:
+	// 			console.log(opcao);
+	// 	}
+	// }
 
-    switch (opcao) {
-        case 1:
-            const tipo = readlineSync.question('Digite o tipo do novo veiculo (carro/moto): ');
-            const marca = readlineSync.question('Digite a marca do novo veiculo: ');
-            const modelo = readlineSync.question('Digite o modelo do novo veiculo: ');
-            const ano = parseInt(readlineSync.question('Digite o ano do novo veiculo: '));
-            const placa = readlineSync.question('Digite a placa do novo veiculo: ');
-            const diaria = parseInt(readlineSync.question('Digite o valor da diaria para o novo veiculo: '));
-            const disponivel = readlineSync.question('Este veiculo estara disponivel para aluguel? S/N: ');
-            const estaDisponivel = disponivel === 'S' ? true : false
-            const novoVeiculo = new Veiculo(tipo, marca, modelo, ano, placa, diaria, estaDisponivel);
-            novoVeiculo.cadastrarVeiculo(veiculos, tipo, marca, modelo, ano, placa, diaria, estaDisponivel);
-            break;
+	switch (opcao) {
+		case '1':
+		const tipo = readlineSync.question('Digite o tipo do novo veiculo (carro/moto): ');
+		const marca = readlineSync.question('Digite a marca do novo veiculo: ');
+		const modelo = readlineSync.question('Digite o modelo do novo veiculo: ');
+		const ano = parseInt(readlineSync.question('Digite o ano do novo veiculo: '));
+		const placa = readlineSync.question('Digite a placa do novo veiculo: ');
+		const diaria = parseInt(readlineSync.question('Digite o valor da diaria para o novo veiculo: '));
+		const disponivel = readlineSync.question('Este veiculo estara disponivel para aluguel? S/N: ');
+		const estaDisponivel: boolean =( disponivel === 'S' || disponivel === 's') ? true : false;
+		Veiculo.cadastrarVeiculo(tipo, marca, modelo, ano, placa, diaria, estaDisponivel);
+		break;
 
-        case 2:
-            const dataInicio = new Date(readlineSync.question('Digite a data de inicio (YYYY-MM-DD): '));
-            const dataFim = new Date(readlineSync.question('Digite a data de termino (YYYY-MM-DD): '));
-            const cpfCliente = readlineSync.question('Digite o CPF do cliente: ');
-            const placaVeiculo = readlineSync.question('Digite a placa do veiculo que deseja alugar: ');
-            Locadora.cadastrarAluguel(dataInicio, dataFim, cpfCliente, placaVeiculo);
-            break;
+		case '2':
+			const dataInicio = new Date(
+				readlineSync.question("Digite a data de inicio (YYYY-MM-DD): ")
+			);
+			const dataFim = new Date(
+				readlineSync.question("Digite a data de termino (YYYY-MM-DD): ")
+			);
+			const cpfCliente = readlineSync.question("Digite o CPF do cliente: ");
+			const placaVeiculo = readlineSync.question(
+				"Digite a placa do veiculo que deseja alugar: "
+			);
+			Locadora.cadastrarAluguel(dataInicio, dataFim, cpfCliente, placaVeiculo);
+			break;
 
-        case 3:
-            const cpfClienteDevolucao = readlineSync.question('Digite o CPF do cliente: ');
-            const aluguelAtivo = Aluguel.listarAlugueisAtivos().find((aluguel) => aluguel._cliente.cpf === cpfCliente);
-            aluguelAtivo.devolucaoVeiculo(cpfClienteDevolucao);
-            
-        case 4:
-            Veiculo.listarVeiculosDisponiveis(veiculos);
-            break;
+		case '3':
+			const cpfClienteDevolucao = readlineSync.question(
+				"Digite o CPF do cliente: "
+			);
+			const aluguelAtivo = Aluguel.listarAlugueisAtivos().find(
+				(aluguel) => aluguel._cliente.cpf === cpfCliente
+			);
+			aluguelAtivo.devolucaoVeiculo(cpfClienteDevolucao);
 
-        case 5:
-            Veiculo.listarVeiculosAlugados(veiculos);
-            break;
+		case '4':
+			Veiculo.listarVeiculosDisponiveis(veiculos);
+			break;
 
-        case 6:
-            running = false;
-            break;
+		case '5':
+			Veiculo.listarVeiculosAlugados(veiculos);
+			break;
 
-        default:
-            console.log('Opção inválida. Tente novamente.');
-    }
+		case '6':
+			running = false;
+			break;
+
+		default:
+			console.log("Opção inválida. Tente novamente.");
+	}
 }
--- model/Veiculo.ts
+++ model/Veiculo.ts
@@ -5,98 +5,137 @@ const fs = require("fs");
 const path = require("path");
 
 class Veiculo {
-    tipo: string;
-    marca: string;
-    modelo: string;
-    ano: number;
-    placa: string;
-    valorDiaria: number;
-    estaDisponivel: boolean;
+	tipo: string;
+	marca: string;
+	modelo: string;
+	ano: number;
+	placa: string;
+	valorDiaria: number;
+	estaDisponivel: boolean;
 
+	constructor(
+		tipo: string,
+		marca: string,
+		modelo: string,
+		ano: number,
+		placa: string,
+		valorDiaria: number,
+		estaDisponivel: boolean
+	) {
+		this.tipo = tipo;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.placa = placa;
+		this.valorDiaria = valorDiaria;
+		this.estaDisponivel = estaDisponivel;
+	}
 
-    constructor(tipo: string, marca: string, modelo: string, ano: number, placa: string, valorDiaria: number, estaDisponivel:boolean) {
-        this.tipo = tipo;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.placa = placa;
-        this.valorDiaria = valorDiaria;
-        this.estaDisponivel = estaDisponivel;
-    }
+    private static verificarVeiculoExistente(placa: string): boolean {
+        const veiculos = Veiculo.listarVeiculos();
+		return veiculos.some((veiculo) => veiculo.placa === placa);
+	}
 
-    cadastrarVeiculo(veiculos: Veiculo[], tipo: string, marca: string, modelo: string, ano: number, placa: string, valorDiaria: number, estaDisponivel: boolean) {
-        if (!this.veiculoExistente(veiculos, placa)) {
-            const novoVeiculo = new Veiculo(tipo, marca, modelo, ano, placa, valorDiaria, estaDisponivel);
-            try {
+    static listarVeiculos() {
+		try {
+			const filePath = path.join(__dirname, "..", "data", "veiculos.json");
+			const content = fs.readFileSync(filePath, "utf-8");
+			const clientes = JSON.parse(content);
+			return clientes;
+		} catch (error) {
+			console.error("Erro ao ler o arquivo JSON:", error);
+			return [];
+		}
+	}
+
+	static cadastrarVeiculo(
+		tipo: string,
+		marca: string,
+		modelo: string,
+		ano: number,
+		placa: string,
+		valorDiaria: number,
+		estaDisponivel: boolean
+	) {
+        const veiculos = Veiculo.listarVeiculos();
+		if (!Veiculo.verificarVeiculoExistente(placa)) {
+			const novoVeiculo = new Veiculo(
+				tipo,
+				marca,
+				modelo,
+				ano,
+				placa,
+				valorDiaria,
+				estaDisponivel
+			);
+			try {
 				const filePath = path.join(__dirname, "..", "data", "veiculos.json");
-                const content = fs.readFileSync(filePath, 'utf-8');
-                const veiculos = JSON.parse(content);
-                veiculos.push(novoVeiculo);
+				const content = fs.readFileSync(filePath, "utf-8");
+				const veiculos = JSON.parse(content);
+				veiculos.push(novoVeiculo);
 				fs.writeFileSync(filePath, JSON.stringify(veiculos, null, 2));
 			} catch (error) {
 				console.error("Erro ao ler o arquivo JSON:", error);
 			}
-            console.log('\nVeículo cadastrado com sucesso!');
-        } else {
-            console.log('\nJá existe um veículo com essa placa cadastrado.');
-        }
-    }
+			console.log("\nVeículo cadastrado com sucesso!");
+		} else {
+			console.log("\nJá existe um veículo com essa placa cadastrado.");
+		}
+	}
 
-    devolverVeiculo(cpfCliente: string): void {
-        const aluguelAtivo = Aluguel.listarAlugueisAtivos().find(
-            (aluguel) => aluguel._cliente.cpf === cpfCliente
-        );
+	static devolverVeiculo(cpfCliente: string): void {
+		const aluguelAtivo = Aluguel.listarAlugueisAtivos().find(
+			(aluguel) => aluguel._cliente.cpf === cpfCliente
+		);
 
-        if (aluguelAtivo) {
-            aluguelAtivo.atualizarStatus();
+		if (aluguelAtivo) {
+			aluguelAtivo.atualizarStatus();
 
-            if (aluguelAtivo._estaAtivo) {
-                console.log('Veículo devolvido dentro do prazo.');
-            } else {
-                console.log('Veículo devolvido após a data de devolução.');
-            }
+			if (aluguelAtivo._estaAtivo) {
+				console.log("Veículo devolvido dentro do prazo.");
+			} else {
+				console.log("Veículo devolvido após a data de devolução.");
+			}
 
-            console.log('Veículo devolvido com sucesso.');
-        } else {
-            console.log('Não há aluguel ativo para este cliente.');
-        }
-    }
+			console.log("Veículo devolvido com sucesso.");
+		} else {
+			console.log("Não há aluguel ativo para este cliente.");
+		}
+	}
 
-    static listarVeiculosAlugados(veiculos: Veiculo[]) {
-        try {
+	static listarVeiculosAlugados(veiculos: Veiculo[]) {
+		try {
 			const filePath = path.join(__dirname, "..", "data", "veiculos.json");
 			const content = fs.readFileSync(filePath, "utf-8");
 			const veiculos = JSON.parse(content);
 			const veiculosFiltrados = veiculos.filter(
 				(veiculo) => veiculo.estaDisponivel === false
 			);
-            console.log('\nVeículos alugados:');
-            veiculosFiltrados.forEach((veiculo) => console.log(veiculo));
+			console.log("\nVeículos alugados:");
+			veiculosFiltrados.forEach((veiculo) => console.log(veiculo));
 		} catch (error) {
 			console.error("Erro ao ler o arquivo JSON:", error);
 			return [];
 		}
-    }
+	}
 
-    static listarVeiculosDisponiveis(veiculos: Veiculo[]) {
-        try {
+	static listarVeiculosDisponiveis(veiculos: Veiculo[]) {
+		try {
 			const filePath = path.join(__dirname, "..", "data", "veiculos.json");
 			const content = fs.readFileSync(filePath, "utf-8");
 			const veiculos = JSON.parse(content);
 			const veiculosFiltrados = veiculos.filter(
 				(veiculo) => veiculo.estaDisponivel === true
 			);
-            console.log('\nVeículos disponíveis:');
-            veiculosFiltrados.forEach((veiculo) => console.log(veiculo));
+			console.log("\nVeículos disponíveis:");
+			veiculosFiltrados.forEach((veiculo) => console.log(veiculo));
 		} catch (error) {
 			console.error("Erro ao ler o arquivo JSON:", error);
 			return [];
 		}
-    }
+	}
 
-    private veiculoExistente(veiculos: Veiculo[], placa: string): boolean {
-        return veiculos.some((veiculo) => veiculo.placa === placa);
-    }
+	
 }
 
 export default Veiculo;
